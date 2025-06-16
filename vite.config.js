c8fde72795d5d9e02965548ec8ef49c756241a4a
Preetham Bonthala <preethambonthala@Mac.lan>
2025-06-16 17:48:30 -0500

    Add resume.docx for download

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 export default defineConfig(({ command }) => ({
-  //base: "/vspreetham-portfolio/",
+  base: "/vsp-portfolio/#hero",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
